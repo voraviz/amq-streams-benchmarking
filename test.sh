7c6abea0ee4fff7035c51eee9fecc4513f74d760
Jia Jun Ng (JJ) <25560159+jiajunngjj@users.noreply.github.com>
2020-08-28 18:24:33 +0800

    Delete test.sh

--- test.sh
+++
@@ -1,4 +0,0 @@
-echo "Enter the name of the new project unique name, this will be used to create the namespace"
-read tenant
-
-sed -i '' 's/namespace: .*/namespace: '"$tenant"'/' cluster-operator/*RoleBinding*.yaml
