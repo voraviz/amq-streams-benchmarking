48e83627890876669df23958cfface3112a89446
Jia Jun Ng <ngjiajun13@gmail.com>
2020-08-31 09:56:08 +0800

    modified deploy_metrics.sh

--- deploy_metrics.sh
+++ deploy_metrics.sh
@@ -4,7 +4,7 @@ working_dir=`pwd`
 
 echo "Enter the name of the project to deploy Prometheus and Grafana"
 read tenant
-echo"
+echo
 
 sed -i '' 's/namespace: .*/namespace: '"$tenant"'/' metrics/bundle.yaml
 sed -i '' 's/namespace: .*/namespace: '"$tenant"'/' metrics/prometheus.yaml
@@ -19,17 +19,17 @@ echo
 
 echo "Deploy Prometheus"
 
-oc create secret generic additional-scrape-configs --from-file=prometheus-additional.yaml
-oc apply -f strimzi-service-monitor.yaml
-oc apply -f strimzi-pod-monitor.yaml
-oc apply -f prometheus-rules.yaml
-oc apply -f prometheus.yaml
+oc create secret generic additional-scrape-configs --from-file=metrics/prometheus-additional.yaml
+oc apply -f metrics/strimzi-service-monitor.yaml
+oc apply -f metrics/strimzi-pod-monitor.yaml
+oc apply -f metrics/prometheus-rules.yaml
+oc apply -f metrics/prometheus.yaml
  
-echo 
+echo
 
 echo "Deploy Grafana"
 
-oc apply -f grafana.yaml
+oc apply -f metrics/grafana.yaml
 
 echo 
 
