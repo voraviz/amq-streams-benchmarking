829e23f812700bbbada5740e45d2691b13f271ed
wohshon <wohshon@gmail.com>
2020-09-16 11:53:07 +0800

    added jvm

--- test-scripts/run-metrics.sh
+++ test-scripts/run-metrics.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 NAMESPACE=$1
+PROM_URL=$2
 PROCESS_STATE=$(check-process.sh $PERF_ID )
-echo 'POD,CPU(Mi),MEMORY(Mi),TIMESTAMP' > ./$TEST_ID/output.csv
-echo 'POD,CPU(Mi),MEMORY(Mi),TIMESTAMP' > ./$TEST_ID/kafka-0-output.csv
-echo 'POD,CPU(Mi),MEMORY(Mi),TIMESTAMP' > ./$TEST_ID/kafka-1-output.csv
-echo 'POD,CPU(Mi),MEMORY(Mi),TIMESTAMP' > ./$TEST_ID/kafka-2-output.csv
+echo 'POD,CPU(Mi),MEMORY(Mi),JVM(Mi),TIMESTAMP' > ./$TEST_ID/output.csv
+echo 'POD,CPU(Mi),MEMORY(Mi),JVM(MI),TIMESTAMP' > ./$TEST_ID/kafka-0-output.csv
+echo 'POD,CPU(Mi),MEMORY(Mi),JVM(MI),TIMESTAMP' > ./$TEST_ID/kafka-1-output.csv
+echo 'POD,CPU(Mi),MEMORY(Mi),JVM(MI),TIMESTAMP' > ./$TEST_ID/kafka-2-output.csv
 linecpu=0
 totalcpu=0
 linemem=0
@@ -17,7 +18,14 @@ while
   [[ "${PROCESS_STATE}" !=  "STOPPED" ]]
 do
   echo Taking snapshot of resource utilization - $counter to $TEST_ID/output.csv
-  oc adm top pods -n $NAMESPACE -l 'statefulset.kubernetes.io/pod-name in (my-cluster-kafka-0, my-cluster-kafka-1, my-cluster-kafka-2)' | sed 1d | awk -v timestamp=$(date +%F:%T)  'BEGIN{FS=OFS=timestamp} {print $0 OFS value}' | awk -F' ' 'BEGIN{OFS=",";} {gsub(/[Mi]/,""); print $1,$2,$3,$4;}' | awk '{gsub(/m,/,","); print }' >> ./$TEST_ID/output.csv 
+  # oc adm top pods -n $NAMESPACE -l 'statefulset.kubernetes.io/pod-name in (my-cluster-kafka-0, my-cluster-kafka-1, my-cluster-kafka-2)' | sed 1d |awk -F' ' '{print $0, "F11\t"}' | awk -F ' ' -v timestamp=$(date +%F:%T)  '{print $0 timestamp}' | awk -F' ' 'BEGIN{OFS=",";} {gsub(/[Mi]/,""); print $1,$2,$3,$4,$5;}' | awk '{gsub(/m,/,","); print }' >> ./$TEST_ID/output.csv 
+  #http://prometheus-operated-kafka-cluster-1.apps.cluster-83a6.83a6.example.opentlc.com/api/v1/query
+  JVM=$(curl -s  $PROM_URL?query="sum(jvm_memory_bytes_used\{namespace='$NAMESPACE',kubernetes_pod_name=~'my-cluster-kafka-0',strimzi_io_name='my-cluster-kafka'\})" | jq -r ".data.result"[].value |  jq -r '.[]' | sed 1d)  
+  oc adm top pods -n $NAMESPACE -l 'statefulset.kubernetes.io/pod-name in (my-cluster-kafka-0)' | sed 1d |awk -F' ' -v jvm=$JVM '{print $0, jvm"\t" }' | awk -F ' ' -v timestamp=$(date +%F:%T)  '{print $0 timestamp}' | awk -F' ' 'BEGIN{OFS=",";} {gsub(/[Mi]/,""); print $1,$2,$3,$4,$5;}' | awk '{gsub(/m,/,","); print }' >> ./$TEST_ID/output.csv 
+  JVM=$(curl -s  $PROM_URL?query="sum(jvm_memory_bytes_used\{namespace='$NAMESPACE',kubernetes_pod_name=~'my-cluster-kafka-1',strimzi_io_name='my-cluster-kafka'\})" | jq -r ".data.result"[].value |  jq -r '.[]' | sed 1d)  
+  oc adm top pods -n $NAMESPACE -l 'statefulset.kubernetes.io/pod-name in (my-cluster-kafka-1)' | sed 1d |awk -F' '  -v jvm=$JVM  '{print $0, jvm"\t" }' | awk -F ' ' -v timestamp=$(date +%F:%T)  '{print $0 timestamp}' | awk -F' ' 'BEGIN{OFS=",";} {gsub(/[Mi]/,""); print $1,$2,$3,$4,$5;}' | awk '{gsub(/m,/,","); print }' >> ./$TEST_ID/output.csv 
+  JVM=$(curl -s  $PROM_URL?query="sum(jvm_memory_bytes_used\{namespace='$NAMESPACE',kubernetes_pod_name=~'my-cluster-kafka-2',strimzi_io_name='my-cluster-kafka'\})" | jq -r ".data.result"[].value |  jq -r '.[]' | sed 1d)  
+  oc adm top pods -n $NAMESPACE -l 'statefulset.kubernetes.io/pod-name in (my-cluster-kafka-2)' | sed 1d |awk -F' '  -v jvm=$JVM '{print $0, jvm"\t" }' | awk -F ' ' -v timestamp=$(date +%F:%T)  '{print $0 timestamp}' | awk -F' ' 'BEGIN{OFS=",";} {gsub(/[Mi]/,""); print $1,$2,$3,$4,$5;}' | awk '{gsub(/m,/,","); print }' >> ./$TEST_ID/output.csv 
   if 
     [ $counter -gt $startAvg ]
   then 
@@ -42,3 +50,4 @@ echo $((totalcpu / avgCounter))','$((totalmem / avgCounter)) >>  ./$TEST_ID/clus
 
 echo done metrics at $(date +%F-%T) 
 exit 0
+#| awk -F' ' 'BEGIN{OFS=",";} {gsub(/[Mi]/,""); print $1,$2,$3,$4,$5;}' | awk '{gsub(/m,/,","); print }'
--- test-scripts/run-test.sh
+++ test-scripts/run-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ $# -ne 8 ]
+if [ $# -ne 9 ]
   then
     echo "Enter the kafka following parameters:"
     echo "- Test_ID: any arbituary string to describe your test"
@@ -10,8 +10,9 @@ if [ $# -ne 8 ]
     echo "- throughput"
     echo "- namespace where kafka is deployed"
     echo "- namespace where test client is to be deployed, please create beforehand"
+    echo "- promethius url "
     echo "e.g:"
-    echo "./run-test.sh TestCase-50mil-100k-1024-p1 my-cluster-kafka-bootstrap.kafka-cluster.svc.cluster.local:9092 topic1 1024 50000000 100000 kafka-cluster p1"
+    echo "./run-test.sh TestCase-50mil-100k-1024-p1 my-cluster-kafka-bootstrap.kafka-cluster.svc.cluster.local:9092 topic1 1024 50000000 100000 kafka-cluster p1 http://prometheus-operated-kafka-cluster-1.apps.cluster-83a6.83a6.example.opentlc.com/api/v1/query"
     exit 1
 fi
 
@@ -24,5 +25,5 @@ echo $2 $3 $4 $5 $6 $7 $8
 export PERF_ID=$!
 echo Perf test PID: $PERF_ID
 sleep 1
-./run-metrics.sh $7 &
+./run-metrics.sh $7 $9 &
 exit 0
